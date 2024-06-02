e274d1dc95cdc779f90ae12ba80a0aafe59a3584
Miro Keimiöniemi <miro.keimioniemi@aalto.fi>
2024-06-02 11:53:19 +0300

    Finished midground widget

--- lib/components/midground.dart
+++ lib/components/midground.dart
@@ -1,38 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+// Midground widget consists of three stacked, predefined SVG images, the colors 
+// of which are progressively darker shades of the color provided as a parameter
 class Midground extends StatelessWidget {
-  const Midground({required this.color});
+  
+  // Midground widget constructor with color parameter
+  const Midground({super.key, required this.color});
   final Color color;
+
+  // SVG definitions
   final String svg1 = 
-    '''<svg width="100%" height="374" viewBox="0 0 100% 380" fill="none" xmlns="http://www.w3.org/2000/svg">
+    '''<svg viewBox="0 0 360 374" fill="none" xmlns="http://www.w3.org/2000/svg">
        <path d="M82 43.0833C60.4 36.4833 18.3333 69.6667 0 87.0833V374H360V0C335.667 18.9444 271.4 58.85 209 66.9167C131 77 109 51.3333 82 43.0833Z" fill="#BFBFBF"/>
        </svg>''';
   final String svg2 = 
-    '''<svg width="100%" height="282" viewBox="0 0 360 280" fill="none" xmlns="http://www.w3.org/2000/svg">
-       <path d="M170 36.0187C87 73.3659 39.3333 43.9252 -1 31.6262V282H360V0C307.333 10.5421 267 -7.62799 170 36.0187Z" fill="#8B8B8B"/>
+    '''<svg viewBox="0 0 360 282" fill="none" xmlns="http://www.w3.org/2000/svg">
+       <path d="M170 36.0187C87 73.3659 39.3333 43.9252 0 31.6262V282H360V0C307.333 10.5421 267 -7.62799 170 36.0187Z" fill="#8B8B8B"/>
        </svg>''';
   final String svg3 = 
-    '''<svg width="100%" height="233" viewBox="0 0 360 240" fill="none" xmlns="http://www.w3.org/2000/svg">
+    '''<svg viewBox="0 0 360 233" fill="none" xmlns="http://www.w3.org/2000/svg">
        <path d="M144 49.6958C79.2 26.2329 21 85.5419 0 118.129V233H360V0C315 26.3415 208.8 73.1587 144 49.6958Z" fill="#545454"/>
        </svg>''';
 
+  // Build method for the Midground widget
   @override
   Widget build(BuildContext context) {
+    // Return a stack of the three SVG images with color filters applied.
+
+    // The progressively darker shades are obtained by applying a color filter with the lightness of the color
+    // adjusted as an HSL (Hue, Saturation, Lightness) value and then converting it back to the RGB color.
+
+    // The SVG images are rendered from the string definitions with a width equal to the screen width.
     return Stack(
       alignment: Alignment.bottomCenter,
       children: <Widget>[
         ColorFiltered(
           colorFilter: ColorFilter.mode(HSLColor.fromColor(color).withLightness(0.9).toColor(), BlendMode.srcIn),
-          child: SvgPicture.string(svg1),
+          child: SvgPicture.string(svg1, height: MediaQuery.of(context).size.height / 2, fit: BoxFit.fill,),
         ),
         ColorFiltered(
           colorFilter: ColorFilter.mode(HSLColor.fromColor(color).withLightness(0.8).toColor(), BlendMode.srcIn),
-          child: SvgPicture.string(svg2),
+          child: SvgPicture.string(svg2, height: MediaQuery.of(context).size.height / 2.7, fit: BoxFit.fill,),
         ),
         ColorFiltered(
           colorFilter: ColorFilter.mode(HSLColor.fromColor(color).withLightness(0.7).toColor(), BlendMode.srcIn),
-          child: SvgPicture.string(svg3),
+          child: SvgPicture.string(svg3, height: MediaQuery.of(context).size.height / 3.2, fit: BoxFit.fill,),
         ),
       ],
     );
--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,14 @@ import 'components/midground.dart';
 void main() {
   runApp(
     ChangeNotifierProvider(
-      create: (context) => lampState(),
+      create: (context) => LampState(),
       child: const LEDZeppelinApp(),
     ),
   );
 }
 
 // App state
-class lampState extends ChangeNotifier {
-
+class LampState extends ChangeNotifier {
   // Lamp state variables
   bool _isOn = false;
   int _brightness = 100;
@@ -63,42 +62,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      //   title: Text(widget.title),
-      // ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Stack(
+        children: <Widget>[
+          // Midground widget is the topmost background element
+          Expanded(
+            child: Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: Midground(color: Provider.of<LampState>(context).color),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            
-            Midground(color: Provider.of<lampState>(context).color),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
