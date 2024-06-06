ee6518af17aff4617144669362ad035229438bab
Miro Keimiöniemi <miro.keimioniemi@aalto.fi>
2024-06-06 03:51:28 +0300

    Functional Bluetooth connection !!!!!!

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
-import 'package:led_zeppelin_app/components/alarm_animations_list.dart';
-import 'package:led_zeppelin_app/components/celestial.dart';
-import 'package:led_zeppelin_app/components/next_alarm.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:provider/provider.dart';
 
+import 'components/alarm_animations_list.dart';
+import 'components/celestial.dart';
+import 'components/next_alarm.dart';
 import 'components/gradient_background.dart';
 import 'components/midground.dart';
 
@@ -23,12 +26,105 @@ void main() {
 
 // App state
 class LampState extends ChangeNotifier {
+  Timer? _debounce;
   // Lamp state variables
   bool _isOn = true;
   double _brightness = 1;
   Color _color = Colors.white;
   int _selectedAnimation = 1;
   DateTime? _nextAlarm;
+  BluetoothDevice? _connectedDevice;
+  // BluetoothCharacteristic? _controlCharacteristic;
+
+  BluetoothCharacteristic? _isOnCharacteristic;
+  BluetoothCharacteristic? _brightnessCharacteristic;
+  BluetoothCharacteristic? _colorCharacteristic;
+  BluetoothCharacteristic? _selectedAnimationCharacteristic;
+  BluetoothCharacteristic? _nextAlarmCharacteristic;
+
+  final Guid _isOnUuid = Guid('dfc1a400-3523-4626-bd77-3469dbed8b74');
+  final Guid _brightnessUuid = Guid('05f52bf8-4823-42c6-8647-dc89b76ad4e4');
+  final Guid _colorUuid = Guid('b2516e35-6917-43b7-8cad-c7065a9e0033');
+  final Guid _selectedAnimationUuid = Guid('0d72cbb7-742f-4030-b4ec-3aefb8c1eb1a');
+  final Guid _nextAlarmUuid = Guid('');
+
+  LampState() {
+    FlutterBluePlus flutterBlue = FlutterBluePlus();
+
+    FlutterBluePlus.startScan(timeout: const Duration(seconds: 60));
+    FlutterBluePlus.scanResults.listen((results) {
+      for (ScanResult r in results) {
+        print('${r.device.platformName} found! rssi: ${r.rssi}');
+        if (r.device.platformName == 'LED Zeppelin') {
+          print("connecting to ${r.device.platformName}");
+          FlutterBluePlus.stopScan(); 
+          _connectToDevice(r.device);
+          break;
+        }
+      }
+    });
+  }
+
+  Future<void> _connectToDevice(BluetoothDevice device) async {
+  _connectedDevice = device;
+  for (int attempt = 0; attempt <= 10; attempt++) {
+    try {
+      await device.connect();
+      break; // If the connection is successful, exit the loop
+    } catch (e) {
+      if (attempt == 10) rethrow; // If this was the last attempt, rethrow the exception
+      await Future.delayed(const Duration(seconds: 1)); // Wait for a second before retrying
+    }
+  }
+  List<BluetoothService> services = await device.discoverServices();
+  
+  BluetoothService? targetService;
+  
+  for (BluetoothService service in services) {
+    if (service.uuid.toString() == '6932598e-c4fe-4855-9701-240a78abc000') {
+      targetService = service;
+      break;
+    }
+  }
+
+  if (targetService != null) {
+    for (BluetoothCharacteristic characteristic in targetService.characteristics) {
+      switch (characteristic.uuid.toString()) {
+        case 'dfc1a400-3523-4626-bd77-3469dbed8b74':
+          _isOnCharacteristic = characteristic;
+          break;
+        case '05f52bf8-4823-42c6-8647-dc89b76ad4e4':
+          _brightnessCharacteristic = characteristic;
+          break;
+        case 'b2516e35-6917-43b7-8cad-c7065a9e0033':
+          _colorCharacteristic = characteristic;
+          break;
+        case '0d72cbb7-742f-4030-b4ec-3aefb8c1eb1a':
+          _selectedAnimationCharacteristic = characteristic;
+          break;
+        case '2b3e71d1-4c3e-418e-942b-67f28951c2d3':
+          _nextAlarmCharacteristic = characteristic;
+          break;
+      }
+    }
+  }
+
+    // if (_controlCharacteristic != null) {
+    //   _sendLampState();
+    // }
+  }
+
+  // void _sendLampState() {
+  //   if (_controlCharacteristic == null) return;
+  //   List<int> values = [
+  //     _isOn ? 1 : 0,
+  //     (_brightness * 100).toInt(),
+  //     _color.red,
+  //     _color.green,
+  //     _color.blue
+  //   ];
+  //   _controlCharacteristic!.write(values);
+  // }
 
   // Getters
   bool get isOn => _isOn;
@@ -41,21 +137,31 @@ class LampState extends ChangeNotifier {
   void toggle() {
     _isOn = !_isOn;
     notifyListeners();
+    if (_isOnCharacteristic != null) _isOnCharacteristic!.write([isOn ? 1 : 0]);
   }
 
   void setBrightness(double brightness) {
     _brightness = brightness;
     notifyListeners();
+    // if (_brightnessCharacteristic != null) _brightnessCharacteristic!.write([(brightness * 250).toInt()]);
+    _debounce?.cancel();
+    _debounce = Timer(const Duration(milliseconds: 5), () {
+      if (_brightnessCharacteristic != null) {
+        _brightnessCharacteristic!.write([(brightness * 250).toInt()]);
+      }
+    });
   }
 
   void setColor(Color color) {
     _color = color;
     notifyListeners();
+    // _sendLampState();
   }
 
   void setNextAlarm(DateTime? nextAlarm) {
     _nextAlarm = nextAlarm;
     notifyListeners();
+    // _sendLampState();
   }
 
   Future<void> selectTime(BuildContext context) async {
@@ -153,7 +259,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ),
           // Midground widget is the topmost background element
-          Expanded(
+          Positioned(
             child: Align(
               alignment: FractionalOffset.bottomCenter,
               child: Consumer<LampState>(
